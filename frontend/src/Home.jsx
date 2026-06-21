3ed750515cb3f32398bf02b995a05bfbe38cd645
Freniel Zabala <frenielzabala@Freniels-MacBook-Pro.local>
2026-06-20 19:13:29 -0700

    build ready

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -3,7 +3,7 @@ import Profile from './Profile.jsx';
 import { isAuthenticated } from '../util/auth.js';
 import './Home.css';
 
-function  Home() {
+function Home() {
   return (
       <div className="content center-content">
         {isAuthenticated() ? <Profile/>: <Auth/>}
