f89f21ee451a35e7690397992faaced2bcf4f87c
Freniel Zabala <frenielzabala@Freniels-MacBook-Pro.local>
2026-06-22 15:06:13 -0700

    hotfix

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -4,6 +4,9 @@ import { isAuthenticated } from '../util/auth.js';
 import './Home.css';
 
 function Home() {
+  const redirectBase =
+        import.meta.env.MODE === "production" ? PROD_BASE_URL : BASE_URL
+  console.log(redirectBase)
   return (
       <div className="content center-content">
         {isAuthenticated() ? <Profile/>: <Auth/>}
