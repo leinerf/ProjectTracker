6f903fae20afcb2858416404f120bc3eaaf1f8e1
Freniel Zabala <frenielzabala@Freniels-MacBook-Pro.local>
2026-06-22 22:50:10 -0700

    testing env variables

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -4,6 +4,9 @@ import { isAuthenticated } from '../util/auth.js';
 import './Home.css';
 
 function Home() {
+  console.log(import.meta.env.MODE)
+  console.log(PROD_BASE_URL)
+  console.log(BASE_URL)
   return (
       <div className="content center-content">
         {isAuthenticated() ? <Profile/>: <Auth/>}
