580d8e81215143a4b70f249671e0655ac7c8f981
Freniel Zabala <frenielzabala@Freniels-MacBook-Pro.local>
2026-06-22 23:31:06 -0700

    updated frontend to not have random console.log

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -4,9 +4,6 @@ import { isAuthenticated } from '../util/auth.js';
 import './Home.css';
 
 function Home() {
-  console.log(import.meta.env.MODE)
-  console.log(PROD_BASE_URL)
-  console.log(BASE_URL)
   return (
       <div className="content center-content">
         {isAuthenticated() ? <Profile/>: <Auth/>}
