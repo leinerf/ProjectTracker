004a18c48219161396f95562a05be6a3bd114e45
Freniel Zabala <frenielzabala@Freniels-MacBook-Pro.local>
2026-07-13 22:20:44 -0700

    remove unnecessary code

--- frontend/src/Project.jsx
+++ frontend/src/Project.jsx
@@ -1,8 +1,6 @@
 /* eslint-disable react-hooks/set-state-in-effect */
 import { useEffect, useState } from 'react';
-import Button from "react-bootstrap/Button";
 import {  useParams } from 'react-router';
-import { formatDigit, hourMinSecondsMilli } from "../util";
 import { getProject } from '../util/api';
 
 
@@ -43,24 +41,6 @@ function Project() {
                 {tab === "session" && <ProjectSession projectId={id}/>}
                 {tab === "history" && <ProjectHistory projectId={id} tab={tab}/>}
             </div>
-            {/* <div className="mb-3">
-                <p>Description: {project.description ? project.description.substring(0, 200) + (project.description.length > 100 ? '...' : '') : 'No description available'}</p>
-                <Button variant="dark" className="align-icon " onClick={() => showInfoModel(project)}> Expand Description</Button>
-            </div>
-            <div className="mb-3">
-                <p>Total Time Spent: <span className="fw-bold">{formatDigit(hour)}:{formatDigit(min)}:{formatDigit(sec)}:{formatDigit(milliseconds).substring(0,2)}</span></p>
-            </div>
-            <hr className="thick-hr long-hr"/>
-            <div className="row-container">
-                <h1 className="tab" onClick={() => {showTaskType("inProgress")}} style={taskType === "inProgress" ? {textDecoration: "underline"} : null} >InProgress</h1>
-                <h1 className="tab" onClick={() => {showTaskType("finished")}} style={taskType === "finished" ? {textDecoration: "underline"} : null}>Finished</h1>
-            </div>
-            <div className="fixed-height">    
-                <div className="mt-3">
-                    {taskType === "inProgress" ?  activeTasksList(): null}
-                    {taskType === "finished" ? createTaskListByDate() : null}
-                </div>
-            </div> */}
         </div>
     </>
 }
